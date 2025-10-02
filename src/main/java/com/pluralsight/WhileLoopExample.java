523638186b6ea7c0b816546a0a510b36b83ce7d8
Craig McKeachie <craigdmckeachie@gmail.com>
2025-10-02 15:49:17 -0400

    while loop example

---
+++ src/main/java/com/pluralsight/WhileLoopExample.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class WhileLoopExample {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+
+        while (count <= 5) {
+            System.out.println(count);
+            count = count + 1;
+        }
+
+    }
+}
